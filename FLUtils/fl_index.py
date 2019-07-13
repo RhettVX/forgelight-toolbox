2919ab4ed95363ba5e902d0ddd9cc4b270baa3f0
Rhett <reticentcool@protonmail.com>
2019-07-13 14:20:26 -0400

    Started to switch to dataclasses for entries.

--- FLUtils/fl_index.py
+++ FLUtils/fl_index.py
@@ -1,21 +1,47 @@
 from argparse import ArgumentParser
 from binascii import crc32
-from collections import namedtuple
-from os import makedirs, walk
+import os
 from pathlib import Path
-from typing import List
-
-import dataclasses
+from typing import List, Set, Any, Union, AnyStr, Optional, Dict, Tuple
+from dataclasses import dataclass, field
 
 from DbgPack import AssetManager
-
-# TODO: Add method for comparison
-# TODO: Use dataclasses for storing indices
-
-Entry = namedtuple('Entry', 'name name_hash crc32 path')
+from DbgPack.hash import crc64
 
 
-def compare_dumps(path_old: str, path_new: str) -> None:
+@dataclass
+class IndexEntry:
+    name: str = field(default='')
+    name_hash: str = field(default=None)
+    crc32_: Optional[int] = field(default=None)
+    path: Path = field(default=None)
+    subpath: Path = field(default_factory=Path)
+
+    def __post_init__(self):
+        assert self.name or self.name_hash, 'name or name_hash is required'
+        assert self.path, 'these arguments are required'
+
+        if not self.name_hash and self.name:
+            self.name_hash = crc64(self.name)
+
+        # If subpath is a file, then we are probably in a pack
+        # TODO: and empty files will have a crc32 of 0, so we don't want to regenerate that
+        if not self.crc32_ and self.name and self.path and self.subpath.is_dir():
+            self.crc32_ = crc32((self.path / self.subpath / self.name).read_bytes())
+
+
+@dataclass
+class Index:
+    entries: Dict[int, IndexEntry] = field(default_factory=dict)
+
+    def add(self, item):
+        self.entries[hash((item.name_hash, item.subpath))] = item
+
+    def __iter__(self):
+        return iter(self.entries.values())
+
+
+def compare_dumps(path_old, path_new: str) -> None:
     """
     Compare to index dumps and create a difference report
 
@@ -23,56 +49,50 @@ def compare_dumps(path_old: str, path_new: str) -> None:
     :param path_new:  Path to newer index dump
     :return: None
     """
-
-    # Load old index
-    with open(path_old, 'r') as file_old:
-        index_old = [Entry(*l.strip().split(';')) for l in file_old]
-
-    # Load new index
-    with open(path_new, 'r') as file_new:
-        index_new = [Entry(*l.strip().split(';')) for l in file_new]
+    pass
 
 
-def load_files(path: str, namelist: str = None) -> List[Entry]:
+def load_files(path: Path, namelist: Path = None) -> Index:
     """
 
     :param path: Path of root dir to index
     :param namelist: Path to external namelist
-    :return:
+    :return: Set[IndexEntry]
     """
+    index = Index()
+    packs: List[Tuple] = []
 
-    packs = []
-    entries = []
-    namelist_ = []
-
-    if namelist:
-        with open(namelist, 'r') as in_file:
-            namelist_ = [line.strip() for line in in_file]
-
-    print('Indexing files...')
-    for root, _, files in walk(path):
+    print(f'Indexing {path}...')
+    path_len = len(str(path))+1
+    for root, _, files in os.walk(path):
         for file in files:
-            file_path = Path(root, file)
+            subpath = Path(root[path_len:])
+            fullpath = path / subpath / file
 
-            if file_path.suffix in ('.pack', '.pack2'):
-                packs.append(file_path)
+            if fullpath.suffix in ('.pack', '.pack2'):
+                packs.append((path, subpath, file))
+
             else:
-                with open(file_path, 'rb') as in_file:
-                    checksum = crc32(in_file.read())
+                e = IndexEntry(name=file, path=path, subpath=subpath)
+                index.add(e)
 
-                entries.append(Entry(file_path.name, 'None', checksum, file_path))
+    for i in index:
+        print(i)
 
     if packs:
         print('Indexing packs...')
-        am = AssetManager(packs, namelist=namelist_)
+
+        am = AssetManager([str(Path(*p)) for p in packs])
         for a in am:
-            entries.append(Entry(a.name if a.name else 'None', a.name_hash, a.crc32, a.path))
+            e = IndexEntry(name=a.name, name_hash=a.name_hash, crc32_=a.crc32,
+                           path=Path(a.path[:path_len]), subpath=Path(a.path[path_len:]))
+            index.add(e)
 
     print('Done\n')
-    return entries
+    return index
 
 
-def dump_index(index: List[Entry], name: str, dir_: str) -> None:
+def dump_index(index: List[Any], name: str, dir_: str) -> None:
     """
 
     :param index: List of index entries to dump
@@ -80,15 +100,15 @@ def dump_index(index: List[Entry], name: str, dir_: str) -> None:
     :param dir_: Directory to store index dump in
     :return: None
     """
-
-    print('Dumping index...')
-    makedirs(dir_, exist_ok=True)
-    file_path = Path(dir_, name)
-    with open(file_path, 'w') as out_file:
-        for entry in index:
-            out_file.write(f'{entry.name};{entry.name_hash};{entry.crc32};{entry.path}\n')
-
-    print('Done\n')
+    pass
+    # print('Dumping index...')
+    # makedirs(dir_, exist_ok=True)
+    # file_path = Path(dir_, name)
+    # with open(file_path, 'w') as out_file:
+    #     for entry in index:
+    #         out_file.write(f'{entry.name};{entry.name_hash};{entry.crc32};{entry.path}\n')
+    #
+    # print('Done\n')
 
 
 if __name__ == '__main__':
@@ -99,5 +119,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     for path_ in args.dir:
-        arg_path = Path(path_)
-        dump_index(load_files(arg_path, args.namelist), arg_path.name+'.txt', 'Game Indices')
+        load_files(Path(path_), Path(args.namelist))
+        # arg_path = Path(path_)
+        # dump_index(load_files(arg_path, args.namelist), arg_path.name+'.txt', 'Game Indices')
