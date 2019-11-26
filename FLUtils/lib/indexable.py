ad587dcbca75690b7cd20400b584bbbc8efb233a
Rhett <reticentcool@protonmail.com>
2019-11-26 00:31:54 -0500

    Just added a comment for the night

--- FLUtils/lib/indexable.py
+++ FLUtils/lib/indexable.py
@@ -19,7 +19,6 @@ class Indexable:
     comparable: int  # This will probably just be a hash of the content I wanna track
     category: IndexableType = field(default=IndexableType.GENERAL)
     meta_info: Dict[str, str] = field(default_factory=dict)  # Any extra info will go here. I might make Name a
-
     # variable rather than a tag here
 
     # This should compare the comparable, which should be a hash of the content I'm trying to track
@@ -28,5 +27,6 @@ class Indexable:
             return NotImplemented
         return self.comparable == other.comparable and self.unique_id == other.unique_id
 
+    # This is just s shortcut for accessing meta info
     def __getitem__(self, item: str):
         return self.meta_info[item]
